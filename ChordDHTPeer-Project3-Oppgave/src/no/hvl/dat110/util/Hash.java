d26391437d792203ecfe5833242286bef167e203
h591309 <69848522+h591309@users.noreply.github.com>
2022-03-31 12:18:35 +0200

    Oppgave 1 ferdig

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
@@ -6,7 +6,6 @@ package no.hvl.dat110.util;
  *
  */
 
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.net.UnknownHostException;
 import java.security.MessageDigest;
@@ -17,41 +16,72 @@ public class Hash {
 	private static BigInteger hashint; 
 	
 	public static BigInteger hashOf(String entity) {		
-		
 		// Task: Hash a given string using MD5 and return the result as a BigInteger.
+		try {
+			
+			// we use MD5 with 128 bits digest (16 bytes)
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			// compute the hash of the input 'entity'
+			byte[] entityDigest = md.digest(entity.getBytes());
+			// convert the hash into hex format
+			
+			String hashText = toHex(entityDigest);
+			
+			// convert the hex into BigInteger
+			hashint = new BigInteger(hashText, 16);
+			
+			
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 		
-		// we use MD5 with 128 bits digest
 		
-		// compute the hash of the input 'entity'
-		
-		// convert the hash into hex format
-		
-		// convert the hex into BigInteger
 		
 		// return the BigInteger
-		
 		return hashint;
 	}
 	
 	public static BigInteger addressSize() {
 		
+		BigInteger md5AddressSize = null;
+		byte[] digest = null;
 		// Task: compute the address size of MD5
-		
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			digest = md.digest();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// get the digest length
-		
+		int length = digest.length;
 		// compute the number of bits = digest length * 8
-		
+		int numberOfBits = length * 8;
+		BigInteger numberOfBitsBig = new BigInteger("" + numberOfBits);
+		BigInteger two = new BigInteger("2");
 		// compute the address size = 2 ^ number of bits
-		
+		md5AddressSize = two.pow(numberOfBits);
 		// return the address size
 		
-		return null;
+		return md5AddressSize;
 	}
 	
 	public static int bitSize() {
 		
 		int digestlen = 0;
 		
+		byte[] digest = null;
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		digest = md.digest();
+		digestlen = digest.length;
 		// find the digest length
 		
 		return digestlen*8;
